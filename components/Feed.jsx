a2bfe98fe0fda16457f642fb6aebe26d70dd1176
visotoniki9 <d.simon.ssx0@gmail.com>
2022-04-01 21:07:02 +0200

    Fixed spacing bug

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -3,9 +3,9 @@ import Stories from './Stories';
 
 function Feed() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 md:max-w-[975px] lg:grid-col-3 mx-auto ">
+    <div className="grid grid-cols-1 md:grid-cols-3 max-w-[975px] mx-auto ">
       {/* Section */}
-      <section className="col-span-2 mx-auto lg:mx-0">
+      <section className="md:col-span-2 sm:mx-auto">
         <Stories />
         <Posts />
       </section>
