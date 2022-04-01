64d4c005095b02057603b2449b9a641f37b1e996
visotoniki9 <d.simon.ssx0@gmail.com>
2022-04-01 18:24:22 +0200

    bg color gray

--- pages/index.jsx
+++ pages/index.jsx
@@ -4,7 +4,7 @@ import Header from '../components/Header';
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-[#fafafa]">
       <Head>
         <title>Instagram 2.0</title>
         <link rel="icon" href="/instagramIcon.svg" />
