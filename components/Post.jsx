c33378e4e1319fd2a3818b81251ba1e9cfae3a05
visotoniki9 <d.simon.ssx0@gmail.com>
2022-04-01 20:23:15 +0200

    Finished initial posts implementation

--- components/Post.jsx
+++ components/Post.jsx
@@ -1,6 +1,49 @@
-function Post() {
+import {
+  DotsHorizontalIcon,
+  HeartIcon,
+  ChatIcon,
+  PaperAirplaneIcon,
+  BookmarkIcon,
+} from '@heroicons/react/outline';
+
+function Post({
+  post: {
+    username, userImg, img, id,
+  },
+}) {
   return (
-    <div>Post</div>
+    // Post
+    <div className="flex flex-col border max-w-[600px] bg-white rounded">
+      {/* Post Header */}
+      <div className="flex justify-between p-3 items-center">
+        {/* Left  */}
+        <div className="flex space-x-2">
+          {/* UserImage */}
+          <div className="bg-gradient-to-bl from-[#B7318E] to-[#F99034] p-[2px] rounded-full object-contain">
+            <img src={userImg} alt="userImg" className="w-8 h-8 object-cover rounded-full border-[2px] border-white" />
+          </div>
+          {/* Username */}
+          <p className="font-mono font-semibold mt-1">{username}</p>
+        </div>
+        {/* Right */}
+        <DotsHorizontalIcon className="h-5 text-gray-700 cursor-pointer" />
+      </div>
+      {/* MainImg */}
+      <img src={img} alt="img" />
+      {/* Footer */}
+      <div>
+        {/* Icons */}
+        <div className="flex justify-between p-3 text-gray-700">
+          <div className="flex space-x-4">
+            <HeartIcon className="h-6 cursor-pointer" />
+            <ChatIcon className="h-6 cursor-pointer" />
+            <PaperAirplaneIcon className="h-6 cursor-pointer" />
+          </div>
+          <BookmarkIcon className="h-6 cursor-pointer" />
+        </div>
+      </div>
+
+    </div>
   );
 }
 
--- components/Posts.jsx
+++ components/Posts.jsx
@@ -1,8 +1,27 @@
 import Post from './Post';
 
+const posts = [
+  {
+    id: '123',
+    username: 'yuno',
+    userImg: '/dummy.jpg',
+    img: '/dummy.jpg',
+  },
+  {
+    id: '124',
+    username: 'yuno',
+    userImg: '/dummy.jpg',
+    img: '/dummy.jpg',
+  },
+];
+
 function Posts() {
   return (
-    <Post />
+    <div className="mt-8 flex flex-col space-y-4">
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
+    </div>
   );
 }
 
