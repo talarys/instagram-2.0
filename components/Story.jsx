7a232ccc1f26b9ff05741a6132c7fa00fbd388cc
visotoniki9 <d.simon.ssx0@gmail.com>
2022-04-01 20:03:11 +0200

    Fixed border

--- components/Story.jsx
+++ components/Story.jsx
@@ -2,7 +2,7 @@ function Story({ avatarImg, name }) {
   return (
     <div>
       <div className="bg-gradient-to-bl from-[#B7318E] to-[#F99034] p-[2px] rounded-full object-contain">
-        <img className="h-18 w-18 rounded-full p-[1.5px] bg-white object-contain cursor-pointer" src={avatarImg} alt="" />
+        <img className="h-18 w-18 rounded-full border-[2px] object-contain cursor-pointer" src={avatarImg} alt="" />
       </div>
       <p className="w-16 text-center text-xs truncate">{name}</p>
     </div>
